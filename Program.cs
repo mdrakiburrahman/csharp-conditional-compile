c8f390ac7529f2eb6c8428eeac57ec048904b73b
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-08-07 13:03:48 -0400

    Added build and runtime conditionals

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            Console.WriteLine("Running on Linux!");
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            Console.WriteLine("Running on Windows!");
+
+#if LINUX
+        Console.WriteLine("Built on Linux!");
+#elif WINDOWS
+        Console.WriteLine("Built in Windows!");
+#endif
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## References
 
 * [Platform Conditional Compilation in .NET Core](https://blog.magnusmontin.net/2018/11/05/platform-conditional-compilation-in-net-core/)
+* [Selective Propagation of Conditionals](https://stackoverflow.com/a/43442076/8954538)
